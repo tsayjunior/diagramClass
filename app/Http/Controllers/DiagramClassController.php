a003520da3a35a65c9b5995a0351f53fd6da790e
tsayjunior <juniorllanos15@gmail.com>
2024-10-04 11:47:38 -0400

    probando

--- app/Http/Controllers/DiagramClassController.php
+++ app/Http/Controllers/DiagramClassController.php
@@ -27,7 +27,7 @@ class DiagramClassController extends Controller
     public function index(Request $request)
     {
         // return Inertia::location(route('diagram_class.index'));
-        dd($request->code_sala);
+        // dd($request->code_sala);
         $claseController = new ClaseController();
         $array_node = $claseController->format_class_init($request->code_sala);
         $array_links = $claseController->format_relacions_init($request->code_sala);
@@ -37,6 +37,7 @@ class DiagramClassController extends Controller
         $user = User::find($user_auth->id);
         $user->code_sala_activate = $sala->code_sala;
         $user->save();
+        dd($user);
         return Inertia::render('DiagramClass/IndexDiagram',
             [
                 'sala' => $sala,
